5f3a482104c8e0ab734f4008d37ef9203acb9c25
jross1021 <joshuaross798@gmail.com>
2018-04-20 10:39:44 -0700

    Still getting ship to work

--- 122 PA9/main.cpp
+++ 122 PA9/main.cpp
@@ -3,29 +3,31 @@
 const int Width = 900;
 const int Height = 600;
 
+using namespace sf;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(Width, Height), "SFML works!");
+	RenderWindow window(VideoMode(Width, Height), "SFML works!");
 	
-	
+	window.setFramerateLimit(60);
+
 	//Creating a texture for the spaceship using a jpeg file i found
-	sf::Texture t1;
-	t1.loadFromFile("Spaceship.jpg");
+	Texture t1;
+	t1.loadFromFile("Spaceship.png");
 		
 	//Creating a sprite for the ship
 	sf::Sprite ship(t1);
 
-	//This should just set the shape of the sprite for the ship and include the jpeg file to it
-	ship.setTextureRect((sf::IntRect(30, 0, 30, 30)));
-	ship.setOrigin(40, 40);
+	//This should just set the shape of the sprite for the ship and include the png file to it
+	ship.setTextureRect((IntRect(40, 0, 40, 40)));
+	ship.setOrigin(Width/2, Height/2);
 
 	while (window.isOpen())
 	{
-		sf::Event event;
+		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			if (event.type == Event::Closed)
 				window.close();
 		}
 

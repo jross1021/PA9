ce7d3f57a003bf5634ad40849362e4798d3bd06c
jross1021 <joshuaross798@gmail.com>
2018-04-20 12:40:15 -0700

    Ship Created and positioned on the screen

--- 122 PA9/main.cpp
+++ 122 PA9/main.cpp
@@ -7,21 +7,24 @@ using namespace sf;
 
 int main()
 {
-	RenderWindow window(VideoMode(Width, Height), "SFML works!");
-	
-	window.setFramerateLimit(60);
+	//resizing ship
+	Vector2f targetSize(50.0f, 50.0f);
 
+
+
+	RenderWindow window(VideoMode(Width, Height), "SFML works!");	
+	window.setFramerateLimit(60);
 	//Creating a texture for the spaceship using a jpeg file i found
 	Texture t1;
-	t1.loadFromFile("Spaceship.png");
-		
+	t1.loadFromFile("anotherShip.png");
 	//Creating a sprite for the ship
 	sf::Sprite ship(t1);
 
+	ship.setScale(targetSize.x / ship.getGlobalBounds().width, targetSize.y / ship.getGlobalBounds().height);
+	
 	//This should just set the shape of the sprite for the ship and include the png file to it
-	ship.setTextureRect((IntRect(40, 0, 40, 40)));
-	ship.setOrigin(Width/2, Height/2);
-
+	ship.setTexture(t1);
+	ship.setPosition(Width / 2, Height / 2);
 	while (window.isOpen())
 	{
 		Event event;
@@ -30,9 +33,6 @@ int main()
 			if (event.type == Event::Closed)
 				window.close();
 		}
-
-		///
-		//system("dir");
 
 		window.clear();
 		window.draw(ship);
